7d0a3b5371d5f2d77a08e371c7dc93e0f63625f5
joao31007 <megajvt@gmail.com>
2026-05-13 21:15:07 -0300

    resultado final

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ while sistema_vendas:
     print ('3 - Encerrar sistema')
     opcao = int(input('Escolha uma opção: '))
     
-    vendas = opcao == 1
-    bruto_total = 0
-    desconto_total = 0
-    final_total = 0     
+    registro_vendas = []  
 
     if opcao == 1 :
+        vendas = 0
+        bruto_total = 0
+        desconto_total = 0
+        final_total = 0 
+
         nome = input('Nome do produto: ')
         valor_unitario = float(input('Valor unitario: '))
         quantidade = int(input('Quantidade: '))
@@ -39,21 +41,35 @@ while sistema_vendas:
         
         valor_final = valor_bruto - desconto
           
-        vendas += 1
-        bruto_total += valor_bruto
-        desconto_total += desconto  
-        final_total += valor_final
-      
+        registro_vendas.append((nome, quantidade, valor_bruto, desconto, valor_final))
+        
     
         print(f'Desconto aplicado: R$ {desconto: .2f}')
         print(f'valor do desconto: R$ {valor_desconto}')
-        print(f'valor final da venda: R$ {valor_final:.2f}')
+        print(f'valor final da venda: R$ {valor_final:.2f} \n')
         
 
     elif opcao == 2:
+        registro_vendas = opcao == 1 
+       
+        vendas = len(registro_vendas)
+        bruto_total = sum(venda[2] for venda in registro_vendas)
+        desconto_total = sum(venda[3] for venda in registro_vendas)
+        final_total = sum(venda[4] for venda in registro_vendas)
+      
         print(f'Total de vendas realizadas: {vendas}')
         print(f'Total bruto vendido: R$ {bruto_total:.2f}')
         print(f'Total de descontos concedidos: R$ {desconto_total:.2f}')
         print(f'Total líquido vendido: R$ {final_total:.2f}')
+    
+    elif opcao == 3:
+        print('=== resumo final ===')
+        registro_vendas = opcao == 2
+       
+        vendas = len(registro_vendas)
+        bruto_total = sum(venda[2] for venda in registro_vendas)
+        desconto_total = sum(venda[3] for venda in registro_vendas)
+        final_total = sum(venda[4] for venda in registro_vendas)
+      
 
         
