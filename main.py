b728b1cb89ae91e8203ee1d51251167fa3c16fc7
joao31007 <megajvt@gmail.com>
2026-05-20 20:38:09 -0300

    PRONTO

--- main.py
+++ main.py
@@ -1,75 +1,64 @@
 
-sistema_vendas = True
-while sistema_vendas:
-    print ('=== Sistema de vendas ===')
-    print ('1 - registrar venda')
-    print ('2 - ver resumo parcial')
-    print ('3 - Encerrar sistema')
-    opcao = int(input('Escolha uma opção: '))
-    
-    registro_vendas = []  
+vendas = 0
+valor_b= 0
+desconto_total = 0
+final_total = 0
+try:
+    while True:
+        print ('=== Sistema de vendas === \n')
+        print ('1 - registrar venda')
+        print ('2 - ver resumo parcial')
+        print ('3 - Encerrar sistema')
+        opcao = int(input('\nEscolha uma opção: '))
+        
 
-    if opcao == 1 :
-        vendas = 0
-        bruto_total = 0
-        desconto_total = 0
-        final_total = 0 
+        if opcao == 1 :
+            vendas += 1
 
-        nome = input('Nome do produto: ')
-        valor_unitario = float(input('Valor unitario: '))
-        quantidade = int(input('Quantidade: '))
+            nome = input('Nome do produto: ')
+            valor_unitario = float(input('Valor unitario: '))
+            quantidade = int(input('Quantidade: '))
+            valor_bruto = valor_unitario * quantidade
+            valor_b += valor_bruto 
+            print(f'Valor Total: R$ {valor_bruto:.2f}\n')
 
-        valor_bruto = valor_unitario * quantidade
-        print(f'Valor: R$ {valor_bruto:.2f}')
-
-        if valor_bruto < 100:
-            desconto = valor_bruto * 0
-       
-    
-        elif valor_bruto >=100 and valor_bruto < 499.99:
-            desconto = valor_bruto * 0.05
-            valor_desconto = ('5%')
-
-        elif valor_bruto >= 500 and valor_bruto < 999.99:
-            desconto = valor_bruto * 0.10
-            valor_desconto = ('10%')
-
-        else:
-            desconto = valor_bruto * 0.15
-            valor_desconto = ('15%')
             
-        
-        valor_final = valor_bruto - desconto
-          
-        registro_vendas.append((nome, quantidade, valor_bruto, desconto, valor_final))
-        
-    
-        print(f'Desconto aplicado: R$ {desconto: .2f}')
-        print(f'valor do desconto: R$ {valor_desconto}')
-        print(f'valor final da venda: R$ {valor_final:.2f} \n')
-        
 
-    elif opcao == 2:
-        registro_vendas = opcao == 1 
-       
-        vendas = len(registro_vendas)
-        bruto_total = sum(venda[2] for venda in registro_vendas)
-        desconto_total = sum(venda[3] for venda in registro_vendas)
-        final_total = sum(venda[4] for venda in registro_vendas)
-      
-        print(f'Total de vendas realizadas: {vendas}')
-        print(f'Total bruto vendido: R$ {bruto_total:.2f}')
-        print(f'Total de descontos concedidos: R$ {desconto_total:.2f}')
-        print(f'Total líquido vendido: R$ {final_total:.2f}')
-    
-    elif opcao == 3:
-        print('=== resumo final ===')
-        registro_vendas = opcao == 2
-       
-        vendas = len(registro_vendas)
-        bruto_total = sum(venda[2] for venda in registro_vendas)
-        desconto_total = sum(venda[3] for venda in registro_vendas)
-        final_total = sum(venda[4] for venda in registro_vendas)
-      
+        elif opcao == 2:
+            
+
+            if valor_bruto < 100:
+                desconto_total = valor_b* 0
+        
+        
+            elif valor_b >=100 and valor_b < 499.99:
+                desconto_total= valor_b * 0.05
+                valor_desconto = ('5%')
+
+            elif valor_b >= 500 and valor_b < 999.99:
+                desconto_total = valor_b * 0.10
+                valor_desconto = ('10%')
+
+            else:
+                desconto_total = valor_b * 0.15
+                valor_desconto = ('15%')
+                
+            
+            valor_final = valor_b - desconto_total
+            print(f'Total de vendas realizadas: {vendas}')
+            print(f'Total bruto vendido: R$ {valor_b:.2f}')
+            print(f'Total de descontos concedidos: R$ {desconto_total:.2f}')
+            print(f'Total líquido vendido: R$ {valor_final:.2f}\n')
+            
 
         
+        elif opcao == 3:
+            print('=== resumo final ===')
+            print(f'Total de vendas realizadas: {vendas}')
+            print(f'Total bruto vendido: R$ {valor_b:.2f}')
+            print(f'Total de descontos concedidos: R$ {desconto_total:.2f}')
+            print(f'Total líquido vendido: R$ {valor_final:.2f}\n')
+            print('Encerrando sistema...')
+            break
+except ValueError:
+    print('Opção inválida. Por favor, escolha uma opção válida.')
